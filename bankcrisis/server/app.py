f7777370224d5106865e7a05c917021c0ec295ed
Syam-18 <sukeshgrandhisila@gmail.com>
2026-04-08 23:52:53 +0530

    Move all logic into package

--- bankcrisis/server/app.py
+++ bankcrisis/server/app.py
@@ -53,15 +53,10 @@ app = create_app(
 )
 
 
-def main(host: str = "0.0.0.0", port: int = 8000):
+def main(host="0.0.0.0", port=8000):
     import uvicorn
-    uvicorn.run("server.app:app", host=host, port=port)
+    uvicorn.run("bankcrisis.server.app:app", host=host, port=port)
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=8000)
-    args = parser.parse_args()
-    main(port=args.port)
+    main()
